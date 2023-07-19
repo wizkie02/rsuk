7393a5d611720b06746473da2a01f6cb62e76ec0
wizkie02 <114478555+wizkie02@users.noreply.github.com>
2023-07-19 21:44:21 +0700

    Add files via upload

--- 2_Regex.py
+++ 2_Regex.py
@@ -31,14 +31,14 @@ def clean(chuoi):
     return chuoi
     
 # tạo file chứa data cleaned
-f=open("C:/Users/User/Documents/Machine Learning/social listening project/DataCleaned.csv",mode='w',encoding='utf-8')
+f=open("C:/Users/TranDucLam/Downloads/Data/DataCleaned.csv",mode='w',encoding='utf-8')
 f.close()
 # đọc file data raw
-with open('C:/Users/User/Documents/Machine Learning/social listening project/DataCrawl.csv','r',encoding='utf-8') as f:
+with open('C:/Users/TranDucLam/Downloads/Data/DataCrawl.csv','r',encoding='utf-8') as f:
     reader=csv.reader(f)
     l=[row for row in reader]
 # clean data và viết vào file data cleaned
-with open('C:/Users/User/Documents/Machine Learning/social listening project/DataCleaned.csv',mode="a",encoding='utf-8') as out:
+with open('C:/Users/TranDucLam/Downloads/Data/DataCleaned.csv',mode="a",encoding='utf-8') as out:
     writer=csv.writer(out,delimiter=',',lineterminator='\n')
     count = 0
     for i in range(1,len(l)):
